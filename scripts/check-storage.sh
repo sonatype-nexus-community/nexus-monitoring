3e1b32befa38f7156ad51ba0abdd4721a7fa1dfb
srini-hdp <avsrini@gmail.com>
2021-08-20 15:53:20 +1000

    added service checks

--- scripts/check-storage.sh
+++ scripts/check-storage.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # Script to check Nexus data.dir and BlobStore Filesystem usage
-# Can be schduled to run on Nexus node.
+# Expected to be schduled to run on Nexus node.
 
 NEXUS_URL="http://localhost:8081"
 NEXUS_CREDENTIALS="admin:admin123" # update nexus credentials
 ALERTSIZE=104857600 # Checks for min 100GB free
 ALERTPCT=80  # Checks for more than 80% usage
 ALERTMAILID="root@localhost"
+
+f_check_access () {
+   if curl -sk "${NEXUS_URL}" | grep "Nexus Repository" > /dev/null ; then
+      if [ `curl -sk -i -u ${NEXUS_CREDENTIALS} "${NEXUS_URL}/service/rest/v1/status" | grep ^HTTP | cut -f2 -d" "` != 200 ] ; then
+         echo "Access Denied: Check provided credentials" ; exit
+      fi
+   else
+     echo "Check if Nexus is running on provided URL"  ; exit
+  fi
+}
+
+
 
 f_get_datadir () {
   datadir=`curl -sk -X GET  "${NEXUS_URL}/service/rest/atlas/system-information" -H 'accept: application/json' -u ${NEXUS_CREDENTIALS} | grep -w karaf.data | grep -v uri | cut -d"\"" -f4 | uniq`
@@ -42,12 +54,15 @@ f_send_email() {
 
 # Main
 
+f_check_access
 f_get_datadir
 f_get_blobs
 
+# Check if mount points are detected
+if [[ ${#mnts[@]} == 0 ]] ; then echo "No FileSystem Mounts found, Exiting..." ; exit ; fi
+
 for fs in `tr ' ' '\n' <<< "${mnts[@]}" | sort -u`; do
  f_check_usage $fs
-#done | tee /tmp/checkstorage-alerts.txt
 done > /tmp/checkstorage-alerts.txt
 
 f_send_email
